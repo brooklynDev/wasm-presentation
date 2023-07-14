e7e208f3880256d378051fb66457f4f95d79aa30
Avrumi Friedman <a.friedman07@gmail.com>
2023-07-14 15:03:12 -0400

    Added c# wasi console app

---
+++ 08-wasi-dotnet-console/WasiConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WasiConsole;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static void Main()
+    {
+       Console.WriteLine($"Running on: {RuntimeInformation.OSArchitecture}"); 
+    }
+}
